f14c85e241fcf123ca0c24deaf8544cc4caa02d9
JedersonLuz <jedersonalpha@gmail.com>
2019-08-12 19:45:28 -0300

    Função de adicionar livro ao database implementada no back-end. Dois livros foram cadastrados para realizar o teste, onde além dos dados registrados no database, foram enviados para o storage suas respectivas capas.

--- PyrebaseConnector.py
+++ PyrebaseConnector.py
@@ -8,7 +8,7 @@ def InitializeApp():
         "authDomain": "bibworld-84e32.firebaseapp.com",
         "databaseURL": "https://bibworld-84e32.firebaseio.com",
         "projectId": "bibworld-84e32",
-        "storageBucket": "",
+        "storageBucket": "bibworld-84e32.appspot.com",
         "messagingSenderId": "923704057508",
         "appId": "1:923704057508:web:1ff8ee30f1bb971c"
     }
@@ -22,23 +22,22 @@ class PyrebaseConnector(object):
         self.firebase = InitializeApp()
         
         # Get a reference to the auth service
-        self.auth = firebase.auth()
+        self.auth = self.firebase.auth()
 
         # Get a reference to the database service
-        self.db = firebase.database()
+        self.db = self.firebase.database()
 
         # Get a reference to the storage service
-        self.storage = firebase.storage()
+        self.storage = self.firebase.storage()
 
 
     # Log the user in application
-    def login(self):
-        email = input("Enter email: ")
-        password = getpass.getpass()
+    def login(self, email, password):
+        # email = input("Enter email: ")
+        # password = getpass.getpass()
 
         try:
-            self.user = auth.sign_in_with_email_and_password(email, password)
-            # print(user)
+            self.user = self.auth.sign_in_with_email_and_password(email, password)
             return 'Ok'
         except Exception as e:
             _error_json = e.args[1]
@@ -46,13 +45,13 @@ class PyrebaseConnector(object):
             return _error['message']
 
     # Create a user
-    def signUp(self):
-        email = input("Enter your email: ")
-        displayName = input('Enter your display name: ')
-        password = input("Enter your password: ")
+    def signUp(self, email, displayName, password):
+        # email = input("Enter your email: ")
+        # displayName = input('Enter your display name: ')
+        # password = input("Enter your password: ")
 
         try:
-            self.user = auth.create_user_with_email_and_password(email, password)
+            self.user = self.auth.create_user_with_email_and_password(email, password)
             data = {
                 "displayName": displayName
             }
@@ -62,3 +61,32 @@ class PyrebaseConnector(object):
             _error_json = e.args[1]
             _error = json.loads(_error_json)['error']
             return _error['message']
+    
+    # Register a book in database
+    def createBook(self, ISBN, title, leadAuthor, numPages, pubDate, pathImg):
+        self.storage.child('images/books/'+str(ISBN)).put(pathImg)
+
+        data = {
+            'ISBN': ISBN,
+            'title': title,
+            'leadAutor': leadAuthor,
+            'numPages': numPages,
+            'pubDate': pubDate,
+            'pathImg': 'images/books/'+str(ISBN),
+        }
+        self.db.child('books').child(ISBN).set(data)
+
+    # Update data from a book
+    def updateBook():
+        pass
+
+    # Search for a book
+    def searchBook():
+        pass
+
+    # Remove a book
+    def removeBook():
+        pass
+
+pc = PyrebaseConnector()
+# pc.createBook(ISBN=9788544103166, title='Jogador Número 1', leadAuthor='Ernet Cline', numPages=464, pubDate='8/9/2018', pathImg='images/jogador_n_1.jpg')
